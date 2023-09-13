16c0d4fd206e56149cf62a028a9e4c3b1656051e
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-09-13 10:59:40 +0600

    custom program.cs not working, commented out

--- GithubMonitorApp/Program.cs
+++ GithubMonitorApp/Program.cs
@@ -1,35 +1,35 @@
-﻿using GithubMonitorApp.Middlewares;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker;
+﻿//using GithubMonitorApp.Middlewares;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.Azure.Functions.Worker;
 
-namespace GithubMonitorApp
-{
-    public static class Program
-    {
-        public static void Main()
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults(worker =>
-                {
-                    //worker.ConfigureSystemTextJson();
-                    worker.UseMiddleware<AuthorizationMiddleware>();
-                })
-                .ConfigureServices(services =>
-                {
-                    services
-                        .AddHttpClient();
-                        //.RegisterAll()
-                        //.AddCors();
-                })
-                .Build();
+//namespace GithubMonitorApp
+//{
+//    public static class Program
+//    {
+//        public static void Main()
+//        {
+//            var host = new HostBuilder()
+//                .ConfigureFunctionsWorkerDefaults(worker =>
+//                {
+//                    //worker.ConfigureSystemTextJson();
+//                    worker.UseMiddleware<AuthorizationMiddleware>();
+//                })
+//                .ConfigureServices(services =>
+//                {
+//                    services
+//                        .AddHttpClient();
+//                        //.RegisterAll()
+//                        //.AddCors();
+//                })
+//                .Build();
 
-            host.Run();
-        }
-    }
-}
+//            host.Run();
+//        }
+//    }
+//}
