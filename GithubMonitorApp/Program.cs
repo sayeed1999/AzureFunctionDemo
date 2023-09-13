c4fc0b0c3582e112ff1fdcab13c356f46da63e87
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-09-13 10:57:51 +0600

    custom program.cs added to azure functions app

---
+++ GithubMonitorApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using GithubMonitorApp.Middlewares;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+
+namespace GithubMonitorApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults(worker =>
+                {
+                    //worker.ConfigureSystemTextJson();
+                    worker.UseMiddleware<AuthorizationMiddleware>();
+                })
+                .ConfigureServices(services =>
+                {
+                    services
+                        .AddHttpClient();
+                        //.RegisterAll()
+                        //.AddCors();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
